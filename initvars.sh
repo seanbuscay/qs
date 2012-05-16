56bd2987cf3b286e7306a271cfc08517c5ad68ad
Sean Buscay <sbuscay@gmail.com>
2012-05-16 15:32:02 -0400

    removed some vars not needed added comments to document the vars

--- initvars.sh
+++ initvars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# This should be your mysql root user password.  
 QS_PASS=quickstart
+# This should be your Linux login name.  Also the name of your home directory.
 QS_USER=quickstart
-#QS_HOMEDIR=quickstart
-#QS_WEBSITES=websites
-#QS_REPOS=repos
-
+# If true, all out bound email will be logged rather than actually sent.  Helpful in a dev enviroment.  
+# Set false to not set this up.
 EMAIL_CATCHER=TRUE
