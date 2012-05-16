8d76cd40e68dff6083c889897fd65b2761a7ca2e
Sean Buscay <sbuscay@gmail.com>
2012-05-16 15:33:00 -0400

    Add comments / documentation

--- qs_install.sh
+++ qs_install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Please make sure you read the README file first.
+
 # Run the following command to install
 # bash ~/quickstart/qs_install.sh
 
+# Include our one time init varaibles.  Make sure you have already edited this file.
 source initvars.sh
 
+# Attempt to add your username as a sudo user who does not need to enter a password.
+# This is a security issue if you are on any public network.  
 echo "${QS_USER} ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/${QS_USER} > /dev/null
 sudo chmod 440 /etc/sudoers.d/${QS_USER}
 
