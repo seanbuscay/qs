b5cc4143599e4002f08b125746566a301e78906b
Sean Buscay <sbuscay@gmail.com>
2012-05-18 17:15:07 -0400

    set the qs_user to default to the logged in user

--- initvars.sh
+++ initvars.sh
@@ -3,7 +3,7 @@
 # This should be your mysql root user password.  
 QS_PASS=quickstart
 # This should be your Linux login name.  Also the name of your home directory.
-QS_USER=quickstart
+QS_USER=$USER
 # If true, all out bound email will be logged rather than actually sent.  Helpful in a dev enviroment.  
 # Set false to not set this up.
 EMAIL_CATCHER=TRUE
