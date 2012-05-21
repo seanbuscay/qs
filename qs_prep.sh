e23f01ca9d3764848dd1b10f3678522feb4364f8
Sean Buscay <sbuscay@gmail.com>
2012-05-20 23:40:39 -0400

    create a prep script to do the work for us

---
+++ qs_prep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir ~/quickstart/local
+mkdir ~quickstart/drush/local
+cp ~/quickstart/initvars.sh ~/quickstart/local/initvars.sh
+cp ~/quickstart/drush/quickstartvars.inc ~/quickstart/local/quickstartvars.inc
